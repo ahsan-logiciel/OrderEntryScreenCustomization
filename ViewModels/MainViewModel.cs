ada7b41055ceea0af9dc4c858c0954f99aec4f72
ahsan-logiciel <muhammad.ahsan@logicielservice.com>
2023-09-14 20:39:58 +0500

    Initial commit

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+using DevExpress.Xpf.LayoutControl;
+using DevExpress.XtraEditors;
+using OrderEntryScreenCustomization.Models;
+using System.Collections.ObjectModel;
+
+namespace OrderEntryScreenCustomization.ViewModels
+{
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        //public FlowLayoutControl FlowLayoutControl = new FlowLayoutControl();
+        private bool isAllowMoveCheck = false;
+        public virtual ObservableCollection<ItemData> HiddenFields { set; get; }
+
+        public MainViewModel()
+        {
+            HiddenFields = new ObservableCollection<ItemData>
+            {
+                new ItemData { Name = "Symbol", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Quantity", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Price", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Account", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Destination", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Text...", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Price Type", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "Stop Px", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "TIF", Value = "", Type = "TextEdit", IsVisible = true },
+                new ItemData { Name = "GTD Date", Value = "", Type = "TextEdit", IsVisible = true }
+            };
+
+        }
+        public virtual bool IsAllowMoveCheck { get => isAllowMoveCheck; set => isAllowMoveCheck = value; }
+    }
+}
